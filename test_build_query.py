29f9952dbad330d18c0789e0e9bd932f3ecf1aa6
Mary Ellen Weber <mmweber2@gmail.com>
2016-10-04 11:02:33 -0500

    Add tests for _calculate_date.

--- test_build_query.py
+++ test_build_query.py
@@ -1,4 +1,5 @@
 from japan_flight_check import _build_query
+from japan_flight_check import _calculate_date
 from nose.tools import assert_equals
 
 def test_build_unchanged():
@@ -11,6 +12,23 @@ def test_build_change_departure_city():
 def test_build_change_arrival_city():
     result = _build_query(arr_port="HND")
     assert_equals(open("change_arr_city.txt", "r").readlines(), result)
+
+def test_calculate_date_unchanged():
+    date = "2016-10-04"
+    assert_equals(_calculate_date(date, 0), date)
+
+def test_calculate_date_same_month():
+    date = "2016-10-04"
+    assert_equals(_calculate_date(date, 2), "2016-10-06")
+
+def test_calculate_date_new_month_30():
+    date = "2016-09-30"
+    assert_equals(_calculate_date(date, 2), "2016-10-02")
+
+def test_calculate_date_new_year():
+    date = "2016-12-30"
+    assert_equals(_calculate_date(date, 3), "2017-01-02")
+
 
 
 
