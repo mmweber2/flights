2759485ddf1a0a7fb5f7e6696ec31c13d84b4fcb
Mary Ellen Weber <mmweber2@gmail.com>
2016-10-04 10:29:55 -0500

    Add test for changing arrival city.

--- test_build_query.py
+++ test_build_query.py
@@ -8,5 +8,9 @@ def test_build_change_departure_city():
     result = _build_query(dep_port="IND")
     assert_equals(open("change_dep_city.txt", "r").readlines(), result)
 
+def test_build_change_arrival_city():
+    result = _build_query(arr_port="HND")
+    assert_equals(open("change_arr_city.txt", "r").readlines(), result)
+
 
 
