265eb8cc337656ca268172d284cbdf0a5aab2b4c
Mary Ellen Weber <mmweber2@gmail.com>
2016-10-07 11:51:31 -0500

    Update tests to use .json files.

--- test_build_query.py
+++ test_build_query.py
@@ -1,17 +1,28 @@
 from japan_flight_check import _build_query
 from japan_flight_check import _calculate_date
+from japan_flight_check import send_request
 from nose.tools import assert_equals
 
 def test_build_unchanged():
-    assert_equals(open("base_query.txt", "r").readlines(), _build_query())
+    result = _build_query()
+    expected = "" 
+    with open("base_query.json", "r") as input_file:
+        expected = "".join(input_file.readlines())
+    assert_equals(expected, result)
 
 def test_build_change_departure_city():
     result = _build_query(dep_port="IND")
-    assert_equals(open("change_dep_city.txt", "r").readlines(), result)
+    expected = "" 
+    with open("change_dep_city.json", "r") as input_file:
+        expected = "".join(input_file.readlines())
+    assert_equals(expected, result)
 
 def test_build_change_arrival_city():
-    result = _build_query(arr_port="HND")
-    assert_equals(open("change_arr_city.txt", "r").readlines(), result)
+    result = _build_query(arr_port="KIX")
+    expected = "" 
+    with open("change_arr_city.json", "r") as input_file:
+        expected = "".join(input_file.readlines())
+    assert_equals(expected, result)
 
 def test_calculate_date_unchanged():
     date = "2016-10-04"
@@ -28,6 +39,17 @@ def test_calculate_date_new_month_30():
 def test_calculate_date_new_year():
     date = "2016-12-30"
     assert_equals(_calculate_date(date, 3), "2017-01-02")
+
+def test_build_change_departure_date():
+    result = _build_query(dep_date="2017-05-01")
+    expected = "" 
+    with open("change_dep_date.json", "r") as input_file:
+        expected = "".join(input_file.readlines())
+    assert_equals(expected, result)
+
+def test_send_request():
+    pass
+    #print send_request()
 
 
 
