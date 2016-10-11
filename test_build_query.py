71f96518823a300e75fa0133c702526b555dd9a3
Mary Ellen Weber <mmweber2@gmail.com>
2016-10-10 21:47:53 -0500

    Add test for changing max cost.

--- test_build_query.py
+++ test_build_query.py
@@ -1,24 +1,24 @@
-from japan_flight_check import _build_query
+from japan_flight_check import build_query
 from japan_flight_check import _calculate_date
 from japan_flight_check import send_request
 from nose.tools import assert_equals
 
 def test_build_unchanged():
-    result = _build_query()
+    result = build_query()
     expected = "" 
     with open("base_query.json", "r") as input_file:
         expected = "".join(input_file.readlines())
     assert_equals(expected, result)
 
 def test_build_change_departure_city():
-    result = _build_query(dep_port="IND")
+    result = build_query(dep_port="IND")
     expected = "" 
     with open("change_dep_city.json", "r") as input_file:
         expected = "".join(input_file.readlines())
     assert_equals(expected, result)
 
 def test_build_change_arrival_city():
-    result = _build_query(arr_port="KIX")
+    result = build_query(arr_port="KIX")
     expected = "" 
     with open("change_arr_city.json", "r") as input_file:
         expected = "".join(input_file.readlines())
@@ -41,16 +41,19 @@ def test_calculate_date_new_year():
     assert_equals(_calculate_date(date, 3), "2017-01-02")
 
 def test_build_change_departure_date():
-    result = _build_query(dep_date="2017-05-01")
+    result = build_query(dep_date="2017-05-01")
     expected = "" 
     with open("change_dep_date.json", "r") as input_file:
         expected = "".join(input_file.readlines())
     assert_equals(expected, result)
 
+def test_build_change_max_cost():
+    result = build_query(max_cost=2000)
+    expected = "" 
+    with open("change_max_cost.json", "r") as input_file:
+        expected = "".join(input_file.readlines())
+    assert_equals(expected, result)
+
 def test_send_request():
+    #send_request(build_query())
     pass
-    #print send_request()
-
-
-
-
