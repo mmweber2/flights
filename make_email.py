69a4435bb274e3f934603225d60b8e9f97345150
Mary Ellen Weber <mmweber2@gmail.com>
2016-10-24 15:38:48 -0500

    Reorganize parameters of make_email.

--- make_email.py
+++ make_email.py
@@ -1,24 +1,27 @@
 from email.mime.text import MIMEText
 import smtplib
 
-def create_email(text_data, recipient):
+def create_email(text_data, recipient, sender="japanflightcheck@gmail.com"):
     """Generate an email containing the given text.
 
     Args:
-        text_data: string, the message to send in the email.
+        text_data: string, the message to write in the email.
 
-        recipient: string, the email address to which the message
-            will be sent. Must be a valid email address.
+        recipient: string, the email address to which the message will be sent.
+            Must be a valid email address.
+
+        sender: string, the Gmail email address from which to send the email.
+            Defaults to japanflightcheck@gmail.com.
 
     Returns:
-        A String conversion of a MIMEText object with the given data.
+        A MIMEText email object with the given data.
     """
-    # TODO: Error checking
-    # No results for this search
     if not text_data:
-        base_msg = ("Sorry, there weren't any results for your flight searches" +
-            " today.\nIf you're in a hurry, you might try increasing the" +
-            " maximum price or maximum flight length in your search.\n")
+        # No results for this search
+        base_msg = ("Sorry, there weren't any results for your flight" +
+            " searches today.\nIf you're in a hurry, you might try increasing" +
+            " or removing the maximum price or maximum flight length in your" +
+            " search.\n")
         subject = "No Japan flights found today."
     else:
         base_msg = ("Hey there! Here are your daily Japan flight search" +
@@ -28,32 +31,28 @@ def create_email(text_data, recipient):
     msg['Subject'] = subject
     msg['From'] = "japanflightcheck@gmail.com"
     msg['To'] = recipient
-    return msg.as_string()
+    return msg
 
-def send_email(message, recipient):
+def send_email(email, filepath):
     """Sends an email message.
     
     Args:
-        message: string, the email message to send.
+        email: MIMEText object, the email to send.
         
-        recipient: string, the email address to which the message
-            will be sent. Must be a valid email address.
+        filepath: string, the system path to a file containing the password
+            for the message sender's email account.
 
     Raises:
         smtplib.SMTPAuthenticationError: The username and password were not
             accepted by the email server, or the email account does not allow
             access from this program.
     """
-    # TODO: Error checking
-    ssl_server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-    ssl_server.ehlo()
-    email_user = "japanflightcheck@gmail.com"
+    # Get sender/receiver data from message
+    recipient = email['To']
+    email_user = email['From']
     email_pwd = ""
-    with open("/Users/Toz/code/gmail.txt", 'r') as filename:
+    with open(filepath, 'r') as filename:
         email_pwd = filename.read().strip()
     ssl_server.login(email_user, email_pwd)
-    ssl_server.sendmail(email_user, recipient, message)
+    ssl_server.sendmail(email_user, recipient, email.as_string())
     ssl_server.close()
-
-# TODO: Find cleaner way of passing recipient to both
-
